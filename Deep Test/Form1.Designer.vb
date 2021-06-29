bf50f0626fe955577f427c7619388e71584b3460
DoubelHsieh <ga2006009451@gmail.com>
2021-06-29 14:25:32 +0800

    shut

--- Deep Test/Form1.Designer.vb
+++ Deep Test/Form1.Designer.vb
@@ -24,24 +24,34 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.shutup = New System.Windows.Forms.CheckedListBox()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PictureBox1
         '
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
-        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Location = New System.Drawing.Point(156, 181)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(128, 91)
+        Me.PictureBox1.Size = New System.Drawing.Size(128, 57)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
+        '
+        'shutup
+        '
+        Me.shutup.FormattingEnabled = True
+        Me.shutup.Location = New System.Drawing.Point(30, 201)
+        Me.shutup.Name = "shutup"
+        Me.shutup.Size = New System.Drawing.Size(120, 21)
+        Me.shutup.TabIndex = 1
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.shutup)
         Me.Controls.Add(Me.PictureBox1)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -51,4 +61,5 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents shutup As CheckedListBox
 End Class
